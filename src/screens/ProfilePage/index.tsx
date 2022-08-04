18e1ab2c82f39d40b488cd89745c2c3ce655bc6b
David Opoku <david.opoku@cyboticx.com>
2022-08-04 09:31:43 +0000

    added sign out

--- src/screens/ProfilePage/index.tsx
+++ src/screens/ProfilePage/index.tsx
@@ -1,10 +1,42 @@
-import { UserCircleIcon } from "../../components/Icons";
+import { AxiosResponse } from "axios";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { LogoutIcon, UserCircleIcon } from "../../components/Icons";
+import Loader from "../../components/Loader";
 import colors from "../../constants/colors";
+import OkResponse from "../../network/responses/OkResponse";
 import { useSelectState } from "../../store/selectors";
+import { authenticationActions } from "../../store/slices/authentication.slice";
+import { userActions } from "../../store/slices/user.slice";
 import classes from "./index.module.scss";
+import API from "../../constants/api";
 
 const ProfilePage = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
   const { user } = useSelectState();
+  const dispatch = useDispatch();
+
+  const signOut = async () => {
+    if (isLoading) {
+      return;
+    }
+    setIsLoading(true);
+
+    try {
+      const response = await API.client.get<any, AxiosResponse<OkResponse>>(
+        "/user/sign-out"
+      );
+      dispatch(authenticationActions.signOut());
+      dispatch(userActions.signOut());
+      setIsLoading(false);
+      return response.data;
+    } catch (error) {
+      // dispatch(authenticationActions.signOut());
+      // dispatch(userActions.signOut());
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={classes["container"]}>
       <div className={classes["profile"]}>
@@ -13,6 +45,12 @@ const ProfilePage = () => {
           className={classes["label"]}
         >{`${user.firstName} ${user.lastName}`}</p>
         <p className={classes["label"]}>{user.email}</p>
+      </div>
+      <div className={classes["menu"]}>
+        <button className={classes["row-item"]} onClick={signOut}>
+          <LogoutIcon width={16} height={16} color={colors.accent} />
+          {isLoading ? <Loader color={colors.primary} /> : <p>Logout</p>}
+        </button>
       </div>
     </div>
   );
